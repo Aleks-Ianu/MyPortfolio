8334392d902f531b968000b2a1f67cbc41398e38
Aleks Ianu <aleksianu@Anas-iMac-2.broadband>
2024-01-09 23:01:47 +0000

    structure completed

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import projects from "../../projects.json"
+import data from "../../projects.json";
 
 function ProjectCard() {
+  const { projects } = data;
   return (
-    <div className="row row-cols-1 row-cols-md-2 g-4">
-      <div className="col">
-        <div className="card">
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
+    <div className="row row-cols-1 row-cols-md-3 g-5">
+      {projects.map((project) => (
+        <div className="col">
+          <div className="card bg-color-" id={project.id}>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <img src={project.img} className="card-img-top h-20" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">{project.title}</h5>
+              <p className="card-text">{project.description}</p>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,21 +2,17 @@ import React from "react";
 import Hero from "../Hero/Hero";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
-
-function Projects () {
-return (
+function Projects() {
+  return (
     <>
-    <Hero backgroundImage="https://cdn-developer-wp.arc.dev/wp-content/uploads/2022/06/coding-programming-project-ideas-1128x635.jpg">
-      <h1>Welcome, My name is Aleks.</h1>
-      <h2>A front end Web Developer</h2>
-      <img alt="Portfolio's Avatar"  src="https://a0.anyrgb.com/pngimg/422/1054/half-body-model-blond-male-models-to-work-in-an-office-man-delayering-cartoon-man-man-avatar-angry-man-snow-man-thumbnail.png" className="avatarImg" />
-    </Hero>
-    <div className="m-4">
-    <ProjectCard />
-    </div>
+      <Hero backgroundImage="https://cdn-developer-wp.arc.dev/wp-content/uploads/2022/06/coding-programming-project-ideas-1128x635.jpg">
+        <h1>Welcome to my Projects Page</h1>
+      </Hero>
+      <div className="m-4">
+        <ProjectCard />
+      </div>
     </>
-)
+  );
 }
-
 
 export default Projects;
